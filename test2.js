cc5be226893dc59d7f1645c1c8cbc167378ea841
Dmitriy <velikashkin@gmail.com>
2018-02-20 11:44:54 +0300

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,14 +1,15 @@
-alert('YES!SSSSSSSSSSSSSSSSSSSSSSS!!');	$('#topicmenu-options_menu2').remove();
-	var dfd = $('#topic_func').position().top
-	var fdf = $('#topic_func').position().left
-	var menus = $('#topicmenu-options_menu').clone();
-	menus.attr('id','topicmenu-options_menu2').css({
-	  'display':'',
-	  'position':'fixed',
-	  'top':dfd,
-	  'left':fdf,
-	})
-	  .appendTo("body");
-	$('body').click(function(){
-	  $('#topicmenu-options_menu2').remove();
-	});
+alert('YES!SSSSSSSSSSSSSSSSSSSSSSS!!');
+$('#topicmenu-options_menu2').remove();
+var dfd = $('#topic_func').position().top
+var fdf = $('#topic_func').position().left
+var menus = $('#topicmenu-options_menu').clone();
+menus.attr('id','topicmenu-options_menu2').css({
+  'display':'',
+  'position':'fixed',
+  'top':dfd,
+  'left':fdf,
+})
+  .appendTo("body");
+$('body').click(function(){
+  $('#topicmenu-options_menu2').remove();
+});
